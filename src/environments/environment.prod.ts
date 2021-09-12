9eafaa8378cf0e13c5f27857e2d4267065582d15
Sushant Bhosale <sush.bhosale46@gmail.com>
2021-09-12 14:27:50 +0530

    code updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import * as moment from 'moment-timezone';
 export const environment = {
   // baseUrl: 'http://127.0.0.1:8080/api',
-  baseUrl: 'http://cpcakeshopapi.somee.com/api',
+  baseUrl: 'https://cpcakeshopapi.somee.com/api',
   socketBaseUrl: '',
   secret: 'TXlEYWFTc3ByaW5nQm9vdA==',
   adminRoles: ['YM MASTER ADMIN', 'FRANKLY MASTER ADMIN'],
